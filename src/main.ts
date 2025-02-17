1449e6504945d44ff6f47a2249976a33a041f242
Prasad <shreya_prasad@baxter.com>
2025-02-17 12:37:25 +0530

    Basic functionalities up and running

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css"; 
 import { v4 as uuid } from "uuid";
-
 import './style.css'
-
 import TaskItem from "./model/TaskItem";
 import TaskListController from "./controller/TaskListController";
 import TaskListView from "./view/TaskListView.ts";
@@ -10,7 +8,7 @@ import TaskListView from "./view/TaskListView.ts";
 const taskListController = new TaskListController();
 const taskListView = new TaskListView(taskListController);
 const todoForm = document.getElementById("todo-form") as HTMLFormElement;
-const clearBtn = document.getElementById("clear-btn") as HTMLButtonElement;
+const clearBtn = document.getElementById("clear-all-btn") as HTMLButtonElement;
 const showCompletedTask = document.getElementById("completed-btn") as HTMLButtonElement;
 const showTaskToComplete = document.getElementById("pending-tasks-btn") as HTMLButtonElement;
 const showAllTask = document.getElementById("view-tasks-btn") as HTMLButtonElement;
@@ -19,10 +17,11 @@ const initApp = () => {
   const allTask = taskListController.getTaskList();
   taskListView.render(allTask);
 };
+initApp();
 
 if (todoForm) {
   todoForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+    e.preventDefault();  // Prevents default form submission 
     const formData = new FormData(todoForm);
     const todoValue = formData.get("new-todo") as string;
     if (todoValue == null || todoValue?.toString().trim() === "") return;
@@ -30,30 +29,27 @@ if (todoForm) {
 
     taskListController.addTask(newTask);
 
-    clearBtn.addEventListener("click", () => {
-      taskListController.clearTask();
-      taskListView.clear();
-    })
-
-    showCompletedTask.addEventListener("click", () => {
-      const completedTask = taskListController.getCompletedTask();
-      taskListView.render(completedTask);
-    })
-
-    showTaskToComplete.addEventListener("click", () => {
-      const taskToComplete = taskListController.getPendingTask();
-      taskListView.render(taskToComplete);
-    })
-
     initApp();
     todoForm.reset();
-  })  
+  });
 }
 
-/*
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    
-  </div>
-`
-*/
+clearBtn.addEventListener("click", () => {
+  taskListController.clearTask();
+  taskListView.clear();
+});
+
+showCompletedTask.addEventListener("click", () => {
+  const completedTask = taskListController.getCompletedTask();
+  taskListView.render(completedTask);
+});
+
+showTaskToComplete.addEventListener("click", () => {
+  const taskToComplete = taskListController.getPendingTask();
+  taskListView.render(taskToComplete);
+});
+
+showAllTask.addEventListener("click", () => {
+  const taskList = taskListController.getTaskList();
+  taskListView.render(taskList);
+});
